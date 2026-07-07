0e6e7640a43bb92868e22c4b1fd96decd8aa9f94
yurasera <yuhaya.lissera@gmail.com>
2026-07-07 12:00:56 +0700

    Initial Commit

---
+++ Flean/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flean
+//
+//  Created by Yuhaya Lissera on 07/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flean/FleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  FleanApp.swift
+//  Flean
+//
+//  Created by Yuhaya Lissera on 07/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
